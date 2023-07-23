033723f12419806e26f754879cb650e43adcd36a
guillaume <guillaume.mucwicki@gmail.com>
2023-07-23 15:36:28 +0200

    refactor: Refactor color constants and adjust table calculations
    - Update color constants to use `lipgloss.AdaptiveColor`
    - Adjust height and width calculations for the table
    - Modify table styles to use constants
    - Update logic for distributing width among columns
    - Add `k.Open` and `k.Up` keys to `ShortHelp` function in `KeyMap` struct
    - Add new modules to `go.mod` and `go.sum`

--- src/tui/constants/styles.go
+++ src/tui/constants/styles.go
@@ -6,16 +6,16 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-const (
-	GREEN      = lipgloss.Color("112")
-	RED        = lipgloss.Color("196")
-	BLUE       = lipgloss.Color("27")
-	YELLOW     = lipgloss.Color("214")
-	BORDER     = lipgloss.Color("240")
-	TEXT       = lipgloss.Color("255")
-	DESC       = lipgloss.Color("245")
-	HIGHLIGHT  = lipgloss.Color("232")
-	BACKGROUND = lipgloss.Color("")
+var (
+	GREEN      = lipgloss.AdaptiveColor{Light: "112", Dark: "112"}
+	RED        = lipgloss.AdaptiveColor{Light: "196", Dark: "196"}
+	BLUE       = lipgloss.AdaptiveColor{Light: "27", Dark: "27"}
+	YELLOW     = lipgloss.AdaptiveColor{Light: "214", Dark: "214"}
+	BORDER     = lipgloss.AdaptiveColor{Light: "0", Dark: "240"}
+	TEXT       = lipgloss.AdaptiveColor{Light: "0", Dark: "255"}
+	DESC       = lipgloss.AdaptiveColor{Light: "244", Dark: "244"}
+	HIGHLIGHT  = lipgloss.AdaptiveColor{Light: "232", Dark: "232"}
+	BACKGROUND = lipgloss.AdaptiveColor{Light: "", Dark: ""}
 )
 
 var HelpStyle = help.Styles{
